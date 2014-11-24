dd1e48e26903fbb0374f3617cd3c6defa09d343f
Maximiliano A Quirino Costa <maxquirinog@gmail.com>
2014-11-23 23:24:17 -0300

    inicio da troca de jogadores

--- dominador.c
+++ dominador.c
@@ -33,6 +33,7 @@
 int main(void)
 {
 	
+	int jogador;
 	int peca;
 	int pp_1,pp_2; //pontas de peca  
 	int pm_1,pm_2; //pontas de mesa
@@ -48,59 +49,60 @@ int main(void)
 
 	// escolha do iniciante
 	
-	printf("jogador 2 sai pq tem o dozao\n");
+	printf("Qual jogador iniciara o jogo? \n");
+	scanf("%d", &jogador);
 
 	while(rodada <= 4)
 	{
-	printf("%s, qual peca vc quer jogar?\n","jogador");
-	scanf("%d",&peca);
+		printf("Jogador %d, qual peca vc quer jogar?\n",jogador);
+		scanf("%d",&peca);
 
-	//separacao das pontas da peca
-	pp_1=peca/10;
-	pp_2=peca-pp_1*10;
+		//separacao das pontas da peca
+		pp_1=peca/10;
+		pp_2=peca-pp_1*10;
 
-	//montagem das pontas de mesa
- 	if(rodada ==1)
-	{
-		pm_1=pp_1;
-		pm_2=pp_2;
-	}
-	else
-	{
-    if((pp_1==pm_1 || pp_1==pm_2) && (pp_2==pm_1 || pp_2==pm_2))
-	{
-		printf("Escolha o lado direito ou esquerdo(d ou e): ");
-		scanf("%c",&lado);
-		if(lado=='d')
+		//montagem das pontas de mesa
+ 		if(rodada ==1)
 		{
-			if(pm_2 == pp_1)	pm_2=pp_2;
-			else pm_2=pp_1;
+			pm_1=pp_1;
+			pm_2=pp_2;
 		}
 		else
 		{
-			if(pm_1 == pp_1) pm_1=pp_2;
-			else pm_1=pp_1;
+    		if((pp_1==pm_1 || pp_1==pm_2) && (pp_2==pm_1 || pp_2==pm_2))
+			{
+				printf("Escolha o lado direito ou esquerdo(d ou e): ");
+				scanf("%c",&lado);
+				if(lado=='d')
+				{
+					if(pm_2 == pp_1)	pm_2=pp_2;
+					else pm_2=pp_1;
+				}
+				else
+				{
+					if(pm_1 == pp_1) pm_1=pp_2;
+					else pm_1=pp_1;
+				}
+			}
+			else if((pp_1!=pm_1 && pp_1!=pm_2)&&(pp_2!=pm_1 && pp_2 !=pm_2))
+		 	{
+		 	// temporariamente vazio	
+		 	}
+		 	else
+		 	{
+				if(pm_1==pp_1) pm_1=pp_2;
+			 	else if(pm_1==pp_2) pm_1=pp_1;
+			 	else if(pm_2==pp_1) pm_2=pp_2;
+		     	else pm_2=pp_1;	
+		 	}
 		}
-	}
-	else if((pp_1!=pm_1 && pp_1!=pm_2)&&(pp_2!=pm_1 && pp_2 !=pm_2))
-		 {
-			
-		 }
-		 else
-		 {
-			 if(pm_1==pp_1) pm_1=pp_2;
-			 else if(pm_1==pp_2) pm_1=pp_1;
-			 	  else if(pm_2==pp_1) pm_2=pp_2;
-				  	   else pm_2=pp_1;	
-		 }
-	}
-	printf("Rodada %d\n",rodada);
-	printf("Pontas: %d | %d\n\n",pm_1, pm_2);
+		printf("Rodada %d\n",rodada);
+		printf("Pontas: %d | %d\n\n",pm_1, pm_2);
     
-	// Teste de fim de jogo
+		// Teste de fim de jogo
 
 
-	rodada +=1;
+		rodada +=1;
 	}//fim do while
 	return 0;
 }

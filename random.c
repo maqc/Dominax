20025bc282fe20c9b04f842edd7b548a880de961
Lucas Canejo Jurema <lucasbrgol2010@hotmail.com>
2014-11-28 19:07:17 -0300

    funcao random para 2 jogadores

--- random.c
+++ random.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-	int y,k;
+	int y=0,x=0,k;
 	int passagens=0, temporario;
 	int peca[28] = {0,10,20,30,40,50,60,11,21,31,41,51,61,22,32,42,52,62,33,43,53,63,44,54,64,55,65,66};
 	int jafoi[28]={0};
@@ -16,14 +16,12 @@ int main(void)
 		temporario= rand()%28;
 		if(jafoi[temporario]==0)
 		{
-
 			jogador1[y]=peca[temporario];
 			jafoi[temporario]=1;
 			passagens++;
 			y++;
 		}
 	}
-	
 	printf("Sua mao:\n");
 	printf("{");
 	for(k=0;k<7;k++)
@@ -35,6 +33,33 @@ int main(void)
 	}
 	printf("}");
 	printf("\n");
-	return 0;
+	
 }
+
+
+	passagens=0;
+	while(passagens!=7)
+    {  
+		   temporario= rand()%28;
+		   if(jafoi[temporario]==0)
+		   {
+		  		 jogador2[x]=peca[temporario];
+			     jafoi[temporario]=1;
+			     passagens++;
+			     x++;
+		   }
+	}	   
+	printf("Sua mao:\n");
+	printf("(");
+	for(k=0;k<7;k++)
+		 {
+		 if(jogador2[k]==0)
+		 	printf("00,");
+	     else
+		 	printf("%d,", jogador2[k]);
+		 }
+		 printf("}")
+		 printf("\n");
+	     return 0;
+
 

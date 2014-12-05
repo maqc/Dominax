5d661ebb99bcc5e7e919b5dbfc1ee93580ae7590
Maximiliano Adolfo Quirino Costa <maxquirinog@gmail.com>
2014-12-05 15:36:10 -0300

    Logotipo para documentação html.

--- doc/html/search/all_64.js
+++ doc/html/search/all_64.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['domino001_2ec',['domino001.c',['../domino001_8c.html',1,'']]],
-  ['domino001',['domino001',['../md_README.html',1,'']]]
+  ['domino001_2ec',['domino001.c',['../domino001_8c.html',1,'']]]
 ];
